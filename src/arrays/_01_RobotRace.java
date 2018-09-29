f61f04fdc97ab3d47f262a4e1df3062588b49ddd
sepfish <league@WTS14.attlocal.net>
2018-09-28 18:29:18 -0700

    Array Cheat Sheet, Robot Race, Find The Hidden Button, Visual Array Sorter

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,46 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
+	public static void main(String[] args) {
 
-	//2. create an array of 5 robots.
+		// 2. create an array of 5 robots.
+		Robot[] robots = new Robot[5];
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot("mini");
+			robots[i].setX(60 * i + 60);
+			robots[i].setSpeed(100000);
+			robots[i].setY(550);
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-	//3. use a for loop to initialize the robots.
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		boolean noWinner = true;
+		Random rand = new Random();
+		while (noWinner) {
+			for (Robot robot : robots) {
+				if (noWinner) {
+					int random = rand.nextInt(50);
+					robot.move(random);
+				}
+				if (robot.getY() < 10) {
+					noWinner = false;
+				}
+			}
+		}
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
+		// 7. declare that robot the winner and throw it a party!
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+		// 8. try different races with different amounts of robots.
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
+		// 9. make the robots race around a circular track.
+	}
 }
